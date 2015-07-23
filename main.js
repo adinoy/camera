55b870448f59238460a22c5bdbd839d215563658
Adi Noy <adinoy@me.com>
2015-07-23 19:50:53 +0300

    updated files

--- main.js
+++ main.js
@@ -1,16 +1,65 @@
-(function ($window, $document){
+(function ($window, $document) {
 
-    var file = q("#capture");
+    var utils = {
+        q: function (selector) {
+            return $document.querySelector(selector);
+        },
+        prepend: function (parent, child) {
+            parent.insertBefore(child, parent.firstChild)
+        },
+        create: function (element) {
+            return $document.createElement(element);
+        }
+    };
 
-    file.addEventListener("input", fileChosen);
+
+    $window.utils = utils;
+
+
+}(window, window.document));
+
+
+
+(function ($window, $document, $) {
+
+    var file = $.q("#capture");
+    var list = $.q(".photos");
+
+    file.addEventListener("change", fileChosen);
 
     function fileChosen (e) {
-        var file = e.target;
-        console.log("User had chosen this file: %o, file")
+        var file = e.target.files[0];
+        addToList(file);
     }
 
-    function q (selector) {
-        return $document.querySelector(selector);
+    function addToList (file) {
+        $.prepend(list, wrapWithTemplate(file));
     }
 
-}(window, window.document))
+    function wrapWithTemplate (file) {
+        var img = wrapWithImg(file);
+        var link = wrapWithLink(img, file);
+        var li = wrapWithLi(link, file);
+        return li;
+    }
+
+    function wrapWithImg (file) {
+        var img = $.create("img");
+        img.alt = file.name;
+        return img;
+    }
+
+    function wrapWithLink (element, file) {
+        var link = $.create("a");
+        link.appendChild(element);
+        return link;
+    }
+
+    function wrapWithLi (element, file) {
+        var li = $.create("li");
+        li.appendChild(element);
+        return li;
+    }
+
+
+}(window, window.document, window.utils));
