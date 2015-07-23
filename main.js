d9230cb9e59c6d0cf686bdbbce7d0b0f84f444ae
Adi Noy <adinoy@me.com>
2015-07-23 18:28:06 +0300

    some changes

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-/**
- * Created by Adinoy on 7/23/15.
- */
+(function ($window, $document){
+
+    var file = q("#capture");
+
+    file.addEventListener("input", fileChosen);
+
+    function fileChosen (e) {
+        var file = e.target;
+        console.log("User had chosen this file: %o, file")
+    }
+
+    function q (selector) {
+        return $document.querySelector(selector);
+    }
+
+}(window, window.document))
